7fc38aada42930469b333d356b53a1bbdf1df183
juanpajedrez <jptriana.martinez@gmail.com>
2026-03-21 15:05:51 -0700

    Adding DocLayNet_linknent_arch.ipynb, with correct classes inside src/models/linknet_model.py and src/models/linknet_layers.py

---
+++ src/models/__init__.py
@@ -0,0 +1,2 @@
+from .linknet_layers import LinknetStem, LinknetEncoderBlock, LinknetDecoderBlock, LinknetReconstructer
+from .linknet_model import LinknetModel
---
+++ src/training/train.py
@@ -0,0 +1,8 @@
+'''
+Author: Juan Pablo Triana Martinez
+Date: 2026-03-20
+The following contains the PyTorch train functions related with the following:
+    - linknet-resnet -> text_detection.
+    - ViTSTR -> text recognition.
+'''
+
